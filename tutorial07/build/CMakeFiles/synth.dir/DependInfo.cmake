7a729ba86f830c8bb16ae1ad654beaf3f298f63c
Sven Wehrend <swehrend@ant-neuro.com>
2026-03-05 18:07:41 +0100

    remove redundant files

---
+++ tutorial07/build/CMakeFiles/synth.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sven/development/cpp-tutorials/tutorial07/Voice.cpp" "CMakeFiles/synth.dir/Voice.cpp.o" "gcc" "CMakeFiles/synth.dir/Voice.cpp.o.d"
+  "/home/sven/development/cpp-tutorials/tutorial07/main.cpp" "CMakeFiles/synth.dir/main.cpp.o" "gcc" "CMakeFiles/synth.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
